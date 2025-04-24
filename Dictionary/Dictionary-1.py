bd8011747fd7272bd2e7b781ce10d16172a5f410
Suhrob1609 <suhroboqmirzayev2000@gmail.com>
2025-04-24 15:42:02 +0500

    Dictionary lessons
    Learning dictionary

--- Dictionary/Dictionary-1.py
+++ Dictionary/Dictionary-1.py
@@ -8,5 +8,19 @@
 # mevalar = {'olma': 10000, 'tarvuz': 8000, 'qovun' : 10000}
 # print(f"Olmaning narxi {mevalar['olma']} so'm ")
 
-talaba_0 = {'ism':'Oqmirzaye Suhrob', 'yosh' : '25', 't_yil' : '2000'}
-print(f"{talaba_0['ism']}, {talaba_0['t_yil']} - yil tug'ilgan, {talaba_0['yosh']} yoshda")
+# talaba_0 = {'ism':'Oqmirzaye Suhrob', 'yosh' : '25', 't_yil' : '2000'}
+# print(f"{talaba_0['ism']}, {talaba_0['t_yil']} - yil tug'ilgan, {talaba_0['yosh']} yoshda")
+# talaba_0['kurs'] = 4
+# talaba_0['fakultet'] = 'informatika'
+# talaba_0['ism'] = 'Suhrob'
+# print(talaba_0)
+
+# bo'sh lug'at
+# talaba_1 = {}
+# talaba_1['ism'] = 'Suhrob Oqmirzayev '
+# talaba_1['kurs'] = 4
+# talaba_1['yosh'] = 25
+# print(talaba_1)
+
+mevalar = {1: 'olma', 2: 'behi', 3: 'anor'}
+print(mevalar.get(4, 'KJKSDF'))
