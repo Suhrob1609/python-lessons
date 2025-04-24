b2acc346be31878f96d4b0f598d11c1be2c9699e
Suhrob1609 <suhroboqmirzayev2000@gmail.com>
2025-04-24 15:09:01 +0500

    Learning dictionary
    Dictionary lesson

---
+++ Dictionary/Dictionary-1.py
@@ -0,0 +1,12 @@
+# car_0 = {'model': 'ferrari', 'rang ' : 'qizil'}
+# print(car_0['model'])
+# print(car_0['rang '])
+
+# en_uz = {'apple':'olma', 'apricot': 'o\'rik', 'banana' : 'banan'}
+# print(en_uz['apple'])
+
+# mevalar = {'olma': 10000, 'tarvuz': 8000, 'qovun' : 10000}
+# print(f"Olmaning narxi {mevalar['olma']} so'm ")
+
+talaba_0 = {'ism':'Oqmirzaye Suhrob', 'yosh' : '25', 't_yil' : '2000'}
+print(f"{talaba_0['ism']}, {talaba_0['t_yil']} - yil tug'ilgan, {talaba_0['yosh']} yoshda")
