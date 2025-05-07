b9ab0b222c303a90ab250466b3cfc7c1d68d391a
Suhrob1609 <suhroboqmirzayev2000@gmail.com>
2025-05-07 09:46:07 +0500

    function
    Function_lesson1

---
+++ function_lesson1.py
@@ -0,0 +1,25 @@
+# def salom_ber():
+#     """ Salom beruvchi funksiya """
+#     print("Assalomu alaykum! ")
+# salom_ber()
+
+# def salom_ber(ism):
+#     """ Foydalanuvchi ismini qabul qilib, unga salom beruvchi funksiya """
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}! ")
+# salom_ber("Suhrob")
+# salom_ber("Shohruh")
+# print(salom_ber.__doc__)
+
+# def toliq_ism(ism, familiya):
+#     """Foydalanuvchining to'liq ism familiyasini chiqaruvchi funksiya """
+#     print(f"Foydalanuvchi ismi: {ism.title()}")
+#     print(f"Foydalanuvchi familiyasi: {familiya.title()}")
+# toliq_ism("Suhrob","Oqmirzayev")
+
+def yosh_hisobla(ism, tugilgan_yil):
+    """Foydalanuvchi yoshini hisoblovchi funksiya"""
+    print(f"Foydalanuvchining ismi: {ism.title()}")
+    print(f"{ism.title()} {2025 - tugilgan_yil} yoshda")
+
+
+yosh_hisobla("Suhrob", 2000)
